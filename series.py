3fc5f0fb7416c79d5d889d3d61feb9bdd5a1a95a
TheMiloAnderson <themiloanderson@gmail.com>
2019-02-19 15:02:49 -0800

    wrote sum_series function, passes all tests

--- series.py
+++ series.py
@@ -17,3 +17,12 @@ def lucas(n):
     for i in range(n - 1):
         first, second = second, first + second
     return first
+
+
+def sum_series(n, start=0):
+    """ accepts integer n & start number, returns nth series number """
+    first, second = start, 1
+    for i in range(n - 1):
+        first, second = second, first + second
+    return first
+    
