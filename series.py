451f00a3991a3381028d861f1f4d46ccb687d0bc
TheMiloAnderson <themiloanderson@gmail.com>
2019-02-19 14:40:46 -0800

    fibonacci function passes all tests

--- series.py
+++ series.py
@@ -5,4 +5,7 @@ Functions that return fibonacci & lucas numbers
 
 def fibonacci(n):
     """ accepts integer n, returns nth fibonacci number """
-    return 5
+    first, second = 0, 1
+    for i in range(n - 1):
+        first, second = second, first + second
+    return first
--- test_series.py
+++ test_series.py
@@ -1,7 +1,17 @@
 from series import fibonacci
 
 
-def test_fibonacci():
+def test_fibonacci_1():
+    actual = fibonacci(1)
+    expected = 0
+    assert actual == expected
+
+def test_fibonacci_2():
+    actual = fibonacci(2)
+    expected = 1
+    assert actual == expected
+
+def test_fibonacci_6():
     actual = fibonacci(6)
     expected = 5
     assert actual == expected
